967db2613e739323f28c3e524cca4de957f9c1f8
Daniela Cardozo <mdanielacardozo@gmail.com>
2024-11-15 12:30:16 -0300

    add home and nav

---
+++ app/Views/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Libreria Bookie">
+    <meta name="keywords" content="Libros, Librería, Agendas">
+    <meta name="author" content="Daniela Cardozo">
+    <title>Librería Bookie</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+    
+        .navbar-custom {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .navbar-nav {
+            flex-direction: row;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white navbar-custom">
+            <div class="navbar-brand"> <img src="img/bookie.png" alt="Logo" style="height: 210px;"> </div>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item"> <a class="nav-link" href="#">Home</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Productos</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Contacto</a> </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
